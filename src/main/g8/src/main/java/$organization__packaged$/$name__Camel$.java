37d48544765d950ab3463ca75d51e9dccda52f3e
Lukasz Konopski <konopski@gmail.com>
2018-05-11 07:16:52 +0200

    init

---
+++ src/main/g8/src/main/java/$organization__packaged$/$name__Camel$.java
@@ -0,0 +1,16 @@
+package $organization$;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class $name;format="Camel"$ {
+    private String message = "hello";
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
